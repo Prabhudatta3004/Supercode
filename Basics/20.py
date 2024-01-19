67bae4800500d2966d5421863172e39448a8eaae
Prabhudatta Mishra <84654664+Prabhudatta3004@users.noreply.github.com>
2024-01-18 19:05:46 -0500

    committed all patterns

--- Basics/20.py
+++ Basics/20.py
@@ -1,7 +1,37 @@
-##question-20
-##here at first we need to print the upper half
-for i in range(10-1):
-    if i <= ceil(10-1/2):
-        for j in range(i+1):
-            print("*", end=" ")
-        for j in range(
+def pattern20(n):
+    # Initializing the spaces
+    spaces = 2 * n - 2
+
+    # Outer loop for printing rows
+    for i in range(1, 2 * n):
+        # Stars for the first half
+        stars = i
+
+        # Stars for the second half
+        if i > n:
+            stars = 2 * n - i
+
+        # Printing the stars
+        for j in range(stars):
+            print("*", end="")
+
+        # Printing the spaces
+        for j in range(spaces):
+            print(" ", end="")
+
+        # Printing the stars
+        for j in range(stars):
+            print("*", end="")
+
+        # Move to the next line
+        print()
+
+        # Adjusting spaces for the next row
+        if i < n:
+            spaces -= 2
+        else:
+            spaces += 2
+
+# Example usage
+N = 5
+pattern20(N)
